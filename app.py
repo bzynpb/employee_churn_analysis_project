157a5ee5fb4cbfe9592fda9ccdb837165efd3b50
bzynpb <90254686+bzynpb@users.noreply.github.com>
2022-07-27 15:34:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ model = pickle.load(open("rf_tuned_model.pkl", "rb"))
 #st.table(df.head())
 #st.dataframe(df.head())
 
-# favicon = st.image("https://github.com/bzynpb/employee_churn_analysis_project/blob/main/work.png")
 st.set_page_config(page_title='Employee Churn Analysis Project', page_icon="👩‍💻")
 
 # st.sidebar.title("Churn Probability of a single Customer")
