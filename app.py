0fac1c45ddd328774a1f8b89f2862f310972f8af
bzynpb <90254686+bzynpb@users.noreply.github.com>
2022-07-27 16:35:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,19 +23,19 @@ html_temp = """
 </div><br>"""
 
 
-with open("style.css") as f:
-    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+# with open("style.css") as f:
+#     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-col1, col2, col3 = st.beta_columns([1,6,1])
+# col1, col2, col3 = st.beta_columns([1,6,1])
 
-with col1:
-    st.write("")
+# with col1:
+#     st.write("")
 
-with col2:
-    image = Image.open("work.png")
+# with col2:
+#     image = Image.open("work.png")
 
-with col3:
-    st.write("")
+# with col3:
+#     st.write("")
 
 
 st.markdown(html_temp,unsafe_allow_html=True)
