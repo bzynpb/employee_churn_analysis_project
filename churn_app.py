aaf88fb5be9b32cd525652bac9e5f10b857a1b0f
bzynpb <90254686+bzynpb@users.noreply.github.com>
2022-07-29 22:07:34 +0100

    Update churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -52,26 +52,35 @@ with col1:
     number_project=st.slider("How many of projects assigned to an employee?", 2 ,7, 5, step=1)
     average_montly_hours=st.slider("How many hours in averega an employee worked in a month?", 96, 310, 200, step=1)
 	
-def slider_color(NB):
 	
-	ColorMinMax = st.markdown(''' <style> div.stSlider > div[data-baseweb = "slider"] > div[data-testid="stTickBar"] > div {
+ColorMinMax = st.markdown(''' <style> div.stSlider > div[data-baseweb = "slider"] > div[data-testid="stTickBar"] > div {
 	    background: rgb(1 1 1 / 0%); } </style>''', unsafe_allow_html = True)
 
 
-	Slider_Cursor = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div[role="slider"]{
+Slider_Cursor = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div[role="slider"]{
 	    background-color: rgb(14, 38, 74); box-shadow: rgb(14 38 74 / 20%) 0px 0px 0px 0.2rem;} </style>''', unsafe_allow_html = True)
 
 
-	Slider_Number = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div > div
+Slider_Number = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div > div
 					{ color: rgb(14, 38, 74); } </style>''', unsafe_allow_html = True)
 
 
-	col = f''' <style> div.stSlider > div[data-baseweb = "slider"] > div > div {{
+col = f''' <style> div.stSlider > div[data-baseweb = "slider"] > div > div {{
 	    background: linear-gradient(to right, rgb(1, 183, 158) 0%, 
-					rgb(1, 183, 158) {NB}%, 
-					rgba(151, 166, 195, 0.25) {NB}%, 
+					rgb(1, 183, 158) {satisfaction_level}%, 
+					rgba(151, 166, 195, 0.25) {satisfaction_level}%, 
 					rgba(151, 166, 195, 0.25) 100%); }} </style>'''
-	ColorSlider = st.markdown(col, unsafe_allow_html = True)   
+
+
+col = f''' <style> div.stSlider > div[data-baseweb = "slider"] > div > div {{
+	    background: linear-gradient(to right, rgb(1, 183, 158) 0%, 
+					rgb(1, 183, 158) {last_evaluation}%, 
+					rgba(151, 166, 195, 0.25) {last_evaluation}%, 
+					rgba(151, 166, 195, 0.25) 100%); }} </style>'''
+
+
+
+ColorSlider = st.markdown(col, unsafe_allow_html = True)   
 
 
 def satisfaction(satisfaction_level): 
